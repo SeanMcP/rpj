f88f8813af149285cdcf9751da2629860ddfc507
Sean McPherson <sean@seanmcp.com>
2023-06-01 06:56:39 -0400

    add deno

---
+++ rpj.deno.js
@@ -0,0 +1,33 @@
+import { parse } from "https://deno.land/std@0.190.0/flags/mod.ts";
+
+const args = parse(Deno.args);
+
+function handleError(e, message) {
+  if (args.verbose) {
+    console.error(e);
+  }
+  console.log(message);
+  Deno.exit(1);
+}
+
+const fieldMap = new Map([
+  ["d", "description"],
+  ["dep", "dependencies"],
+  ["dev", "devDependencies"],
+  ["n", "name"],
+  ["peer", "peerDependencies"],
+  ["v", "version"],
+]);
+
+const text = await Deno.readTextFile("package.json").catch((e) =>
+  handleError(e, "No package.json file found in this directory")
+);
+
+try {
+  const json = JSON.parse(text);
+  const [field = "scripts"] = args._;
+  const property = fieldMap.get(field) || field;
+  console.log(json[property]);
+} catch (e) {
+  handleError(e, "Error parsing package.json");
+}
